8c08d4aa8d7f2a187b827d75cd69c712caa1ffd3
raquel <raquelalvesas@gmail.com>
2023-02-13 20:55:31 -0300

    criacao do projeto

---
+++ src/main/java/com/sistemacontroleestoque/sao/SaoApplication.java
@@ -0,0 +1,13 @@
+package com.sistemacontroleestoque.sao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemacontroleestoque/sao/SaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemacontroleestoque.sao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
